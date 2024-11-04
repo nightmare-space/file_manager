94b38c93e0a1b7a81a556848c277d9d733392f40
nightmare <mengyanshou@gmail.com>
2024-11-04 21:44:51 +0800

    Remove soft video decode

--- lib/view/media-kit/media-kit.dart
+++ lib/view/media-kit/media-kit.dart
@@ -1,68 +1,68 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
-import 'package:global_repository/global_repository.dart';
+// import 'package:dio/dio.dart';
+// import 'package:flutter/material.dart';
+// import 'package:global_repository/global_repository.dart';
 
-// Make sure to add following packages to pubspec.yaml:
-// * media_kit
-// * media_kit_video
-// * media_kit_libs_video
-import 'package:media_kit/media_kit.dart'; // Provides [Player], [Media], [Playlist] etc.
-import 'package:media_kit_video/media_kit_video.dart'; // Provides [VideoController] & [Video] etc.
+// // Make sure to add following packages to pubspec.yaml:
+// // * media_kit
+// // * media_kit_video
+// // * media_kit_libs_video
+// import 'package:media_kit/media_kit.dart'; // Provides [Player], [Media], [Playlist] etc.
+// import 'package:media_kit_video/media_kit_video.dart'; // Provides [VideoController] & [Video] etc.
 
-class MyScreen extends StatefulWidget {
-  const MyScreen({Key? key, required this.url}) : super(key: key);
-  final String url;
-  @override
-  State<MyScreen> createState() => MyScreenState();
-}
+// class MyScreen extends StatefulWidget {
+//   const MyScreen({Key? key, required this.url}) : super(key: key);
+//   final String url;
+//   @override
+//   State<MyScreen> createState() => MyScreenState();
+// }
 
-class MyScreenState extends State<MyScreen> {
-  // Create a [Player] to control playback.
-  late final player = Player();
-  // Create a [VideoController] to handle video output from [Player].
-  late final controller = VideoController(player);
+// class MyScreenState extends State<MyScreen> {
+//   // Create a [Player] to control playback.
+//   late final player = Player();
+//   // Create a [VideoController] to handle video output from [Player].
+//   late final controller = VideoController(player);
 
-  @override
-  void initState() {
-    super.initState();
+//   @override
+//   void initState() {
+//     super.initState();
 
-    MediaKit.ensureInitialized();
-    init();
-  }
+//     MediaKit.ensureInitialized();
+//     init();
+//   }
 
-  init() async {
-    player.open(Media(widget.url));
-    String srtUrl = widget.url.replaceAll(RegExp(r'\.\w+$'), '.srt');
-    Response? response;
-    try {
-      response = await Dio().get(srtUrl);
-      showToast('发现同名字幕文件，加载中...');
-      final String fileContents = response.data ?? '';
-      await player.setSubtitleTrack(SubtitleTrack.data(fileContents));
-    } catch (e) {
-      showToast('未发现同名字幕文件');
-      Log.w('get srt file error $e');
-    }
-  }
+//   init() async {
+//     player.open(Media(widget.url));
+//     String srtUrl = widget.url.replaceAll(RegExp(r'\.\w+$'), '.srt');
+//     Response? response;
+//     try {
+//       response = await Dio().get(srtUrl);
+//       showToast('发现同名字幕文件，加载中...');
+//       final String fileContents = response.data ?? '';
+//       await player.setSubtitleTrack(SubtitleTrack.data(fileContents));
+//     } catch (e) {
+//       showToast('未发现同名字幕文件');
+//       Log.w('get srt file error $e');
+//     }
+//   }
 
-  @override
-  void dispose() {
-    player.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     player.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.width * 9.0 / 16.0,
-        // Use [Video] widget to display video output.
-        child: Video(
-          controller: controller,
-          // controls: CupertinoVideoControls,
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: SizedBox(
+//         width: MediaQuery.of(context).size.width,
+//         height: MediaQuery.of(context).size.width * 9.0 / 16.0,
+//         // Use [Video] widget to display video output.
+//         child: Video(
+//           controller: controller,
+//           // controls: CupertinoVideoControls,
+//         ),
+//       ),
+//     );
+//   }
+// }
--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,20 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <media_kit_libs_linux/media_kit_libs_linux_plugin.h>
-#include <media_kit_video/media_kit_video_plugin.h>
 #include <open_file_linux/open_file_linux_plugin.h>
 #include <screen_retriever/screen_retriever_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 #include <window_manager/window_manager_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) media_kit_libs_linux_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "MediaKitLibsLinuxPlugin");
-  media_kit_libs_linux_plugin_register_with_registrar(media_kit_libs_linux_registrar);
-  g_autoptr(FlPluginRegistrar) media_kit_video_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "MediaKitVideoPlugin");
-  media_kit_video_plugin_register_with_registrar(media_kit_video_registrar);
   g_autoptr(FlPluginRegistrar) open_file_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "OpenFileLinuxPlugin");
   open_file_linux_plugin_register_with_registrar(open_file_linux_registrar);
